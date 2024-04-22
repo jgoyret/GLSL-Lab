21cc38e274e75c89748c01309847cf7ae4d3aaf1
Juan Goyret <j.goyret1475@gmail.com>
2024-04-22 10:05:17 -0300

    changing structure files

---
+++ exp4/shaderLygia.glsl
@@ -0,0 +1,24 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+#define PI 3.1415926535
+
+#include "../assets.glsl"
+#include "../lygia/draw/hex.glsl"
+#include "../lygia/distort/grain.glsl"
+
+void main(){
+    
+    vec2 uv=gl_FragCoord.xy/u_resolution;
+    
+    float color=hex(uv,.5);
+    
+    float color_=grain(vec2(color*u_time),uv*u_time,1.);
+    
+    gl_FragColor=vec4(vec3(color_),1.);
+}
