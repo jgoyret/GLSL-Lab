87046efc227fb2e10c67133e7c3af4d383a01a6f
Juan Goyret <j.goyret1475@gmail.com>
2024-04-22 19:56:11 -0300

    trying waves and mix sb

--- exp2/shaderUno.glsl
+++ exp2/shaderUno.glsl
@@ -1,5 +1,39 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
 
-void main()
-{
-    gl_FragColor= vec4(1.0, 0.1, 0.0,1.0);
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+#define PI 3.1415926535;
+
+#include "../assets.glsl"
+#include "../lygia/filter/fibonacciBokeh.glsl"
+#include "../lygia/distort/grain.glsl"
+
+float plot(vec2 st,float pct){
+    return smoothstep(pct-.02,pct,st.y)-
+    smoothstep(pct,pct+.02,st.y);
+}
+
+void main(){
+    
+    vec2 uv=gl_FragCoord.xy/u_resolution;
+    
+    float y=smoothstep(.1,.9,uv.x);
+    float x=smoothstep(.1,.9,uv.y);
+    
+    vec3 color1=vec3(y);
+    vec3 color2=vec3(x);
+    
+    float pct1=plot(uv,y);
+    float pct2=abs(sin(u_time));
+    
+    color1=(1.-pct1)*color1+pct1*vec3(0.,1.,0.)*u_time*.1;
+    color2=(1.-pct2)*color2+pct2*vec3(1.,0.,0.)*u_time*.1;
+    
+    vec3 color=mix(color1,color2,tan(u_time));
+    
+    gl_FragColor=vec4(color,1.);
 }
