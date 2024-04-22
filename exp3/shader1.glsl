e1dc741f8ce50522c496092b760436493af38513
Juan Goyret <j.goyret1475@gmail.com>
2024-04-22 10:08:43 -0300

    cleaning assets file

--- exp3/shader1.glsl
+++ exp3/shader1.glsl
@@ -8,18 +8,17 @@ uniform float u_time;
 
 #define PI 3.1415926535
 
-#include "./assets.glsl"
+#include "../assets.glsl"
 
-void main() {
-    
-    
-	vec2 uv = gl_FragCoord.xy/u_resolution;
-
-	float color1 = sin((uv.y) * 10.) + sin(5. * 0.05) ;
-	float color2 = triangular((uv.x + 0.25) * pow(uv.x * 100.0, 0.4));
-    float color = mix(color2, color1, cos(5. * PI/9.) / uv.y);
-
-    vec3 colorPal = pal(color, vec3(0.062, 0.218, 0.438), vec3(1.073, 0.114, 0.278), vec3(2.385, 3.138, 0.485), vec3(1.672, 0.502, 1.332));
-
-	gl_FragColor = vec4(colorPal , 1.); 
+void main(){
+	
+	vec2 uv=gl_FragCoord.xy/u_resolution;
+	
+	float color1=sin((uv.y)*10.)+sin(5.*.05);
+	float color2=triangular((uv.x+.25)*pow(uv.x*100.,.4));
+	float color=mix(color2,color1,cos(5.*PI/9.)/uv.y);
+	
+	vec3 colorPal=pal(color,vec3(.062,.218,.438),vec3(1.073,.114,.278),vec3(2.385,3.138,.485),vec3(1.672,.502,1.332));
+	
+	gl_FragColor=vec4(colorPal,1.);
 }
--- exp3/shader2.glsl
+++ exp3/shader2.glsl
@@ -8,20 +8,20 @@ uniform float u_time;
 
 #define PI 3.1415926535
 
-#include "./assets.glsl"
+#include "../assets.glsl"
 
-void main() {
-    
-	vec2 uv = gl_FragCoord.xy/u_resolution;
-
-	float color1 = sin(uv.x * 9.) + sin(u_time) ;
-	float color2 = triangular(uv.y * pow(9., tan(u_time / 2.)));
-	float color3 = square(uv.x * 9.);
-	float colorR = mix(color3, color1, cos(u_time * PI/9.) / uv.x);
-	float colorG = mix(color2, color1, cos(u_time * PI /9.)/ uv.x);
-	float colorB = mix(color3, color2, cos(u_time * PI /9.)/ uv.x);
-
-	vec3 color = vec3(colorR, colorG, colorB);
-
-	gl_FragColor = vec4(color, 1.); 
+void main(){
+	
+	vec2 uv=gl_FragCoord.xy/u_resolution;
+	
+	float color1=sin(uv.x*9.)+sin(u_time);
+	float color2=triangular(uv.y*pow(9.,tan(u_time/2.)));
+	float color3=square(uv.x*9.);
+	float colorR=mix(color3,color1,cos(u_time*PI/9.)/uv.x);
+	float colorG=mix(color2,color1,cos(u_time*PI/9.)/uv.x);
+	float colorB=mix(color3,color2,cos(u_time*PI/9.)/uv.x);
+	
+	vec3 color=vec3(colorR,colorG,colorB);
+	
+	gl_FragColor=vec4(color,1.);
 }
